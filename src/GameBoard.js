de5f591675e4b25c0bd252a799edf0c9116ab56b
wuyuanyuan <wb_wuyuanyuan02@meituan.com>
2025-09-16 19:45:16 +0800

    feat:暂时提交

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,12 +1,54 @@
+function Squares({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  )
+}
+
 function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
+
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+
+    onPlay(nextSquares);
   }
 
-  const nextSquares = squares.slice();
-  if (xIsNext) {
-
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Squares value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Squares value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Squares value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Squares value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Squares value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Squares value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Squares value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Squares value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Squares value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+
 }
